1d100fca84d5a4d8a639d2555e1ce12ad09a90f2
David Stäheli <mistrdave@gmail.com>
2016-12-08 20:08:24 +0100

    function implemented to service :smiley:

--- AutoReservation.Service.Wcf/AutoReservationService.cs
+++ AutoReservation.Service.Wcf/AutoReservationService.cs
@@ -50,67 +50,68 @@ namespace AutoReservation.Service.Wcf
         public void DeleteAuto(AutoDto auto)
         {
             WriteActualMethod();
-            throw new NotImplementedException();
+            businessComponent.DeleteAuto(auto.ConvertToEntity());
         }
 
         public void DeleteKunde(KundeDto kunde)
         {
             WriteActualMethod();
-            throw new NotImplementedException();
+            businessComponent.DeleteKunde(kunde.ConvertToEntity());
         }
 
         public void DeleteReservation(ReservationDto reservation)
         {
             WriteActualMethod();
-            throw new NotImplementedException();
+            businessComponent.DeleteReservation(reservation.ConvertToEntity());
         }
 
         public AutoDto GetAutoById(int id)
         {
             WriteActualMethod();
-            throw new NotImplementedException();
+            return businessComponent.GetAutoById(id).ConvertToDto();
         }
 
         public KundeDto GetKundeById(int id)
         {
             WriteActualMethod();
-            throw new NotImplementedException();
+            return businessComponent.GetKundeById(id).ConvertToDto();
         }
 
         public ReservationDto GetReservationByNr(int reservationsNr)
         {
             WriteActualMethod();
-            throw new NotImplementedException();
+            return businessComponent.GetReservationById(reservationsNr).ConvertToDto();
         }
 
         public AutoDto InsertAuto(AutoDto auto)
         {
             WriteActualMethod();
-            throw new NotImplementedException();
+            return businessComponent.InsertAuto(auto.ConvertToEntity()).ConvertToDto();
         }
 
         public KundeDto InsertKunde(KundeDto kunde)
         {
             WriteActualMethod();
-            throw new NotImplementedException();
+            return businessComponent.InsertKunde(kunde.ConvertToEntity()).ConvertToDto();
         }
 
         public ReservationDto InsertReservation(ReservationDto reservation)
         {
             WriteActualMethod();
-            throw new NotImplementedException();
+            return businessComponent.InsertReservation(reservation.ConvertToEntity()).ConvertToDto();
+            
         }
 
         public void UpdateAuto(AutoDto auto)
         {
             WriteActualMethod();
-            throw new NotImplementedException();
+            businessComponent.UpdateAuto(auto.ConvertToEntity());
         }
 
         public void UpdateKunde(KundeDto kunde)
         {
             WriteActualMethod();
-            throw new NotImplementedException();
+            businessComponent.UpdateKunde(kunde.ConvertToEntity());
         }
 
         public void UpdateReservation(ReservationDto reservation)
